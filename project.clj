d390c531edfc027b23d148d6122960b894d8e3ae
Chris Bui <christopher.d.bui@gmail.com>
2020-01-20 22:25:16 -0600

    Change group to publish non-snapshot

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject tube-alloys/redef-methods "1.0.0-SNAPSHOT"
+(defproject org.christopher-bui/redef-methods "1.0.0"
   :description "Stub clojure multi-method bodies."
   :url "https://github.com/RickMoynihan/redef-methods"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  )
+  :dependencies [[org.clojure/clojure "1.9.0"]])
